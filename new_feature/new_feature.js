b39af7a728e07e16f326f7d4ed77e9f84caafd27
Mark Markov <mmarkov@minervanetworks.com>
2020-11-06 17:01:14 +0200

    New feaure added

---
+++ new_feature/new_feature.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './new_feaure.module.scss'
+
+export default NewComponent = () => (
+  <>
+    <h1 className={styles.blue}>Default Header</h1>
+    <h1 className={styles.error}>Error Header</h1>
+  </>
+)
